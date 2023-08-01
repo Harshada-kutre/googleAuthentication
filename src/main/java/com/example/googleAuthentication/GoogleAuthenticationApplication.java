117269dd1517b10d3feab5d43e542e422f07ea7d
harshada <harshada_kutre@persistent.com>
2023-08-01 13:17:41 +0530

    Home Controller added

---
+++ src/main/java/com/example/googleAuthentication/GoogleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googleAuthentication/GoogleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googleAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
