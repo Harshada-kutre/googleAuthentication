393b45a459419714b64cd4c339898067ca27d561
harshada <harshada_kutre@persistent.com>
2023-08-01 15:16:51 +0530

    HomeControllerTest added

---
+++ src/test/java/com/example/googleAuthentication/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.googleAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class HomeControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldDisplayHomePage() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
